f4aa3db3d43e7618a614c7873155e0c2c65b2ed5
yy933 <116449560+yy933@users.noreply.github.com>
2026-05-26 12:41:06 +0800

    fix: move i18n to correct astro config file

--- astro.config.ts
+++ astro.config.ts
@@ -29,8 +29,8 @@ export default defineConfig({
     }),
   ],
   i18n: {
-    locales: ["en"],
     defaultLocale: "en",
+    locales: ["en", "zh-TW"],
     routing: {
       prefixDefaultLocale: false,
     },
